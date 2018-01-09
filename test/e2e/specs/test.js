6f062c0a1c67ae892875dd9621b19b322343ce0b
jppinal <jupinal@gmail.com>
2018-01-09 12:43:15 +0100

    0.0.3

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -9,11 +9,24 @@ module.exports = {
     const devServer = browser.globals.devServerURL
 
     browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
+      .url(devServer, function(result) {
+        this.verify.urlContains('8080')
+      })
+      .waitForElementVisible('#app', 5000, true, function() {
+        testApp(this)
+      })
   }
+}
+
+const testApp = function(browser) {
+  console.log('testApp')
+  browser.saveScreenshot('test/e2e/testApp.png')
+  browser.expect.element('.hello').to.be.present
+  browser.expect.element('img').to.be.present
+  browser.expect.element('.hello h1').to.be.visible
+  browser.expect.element('.hello h1').text.to.equal('Vuex testing App')
+  browser.expect.element('#vuejs').to.be.an('a')
+  browser.expect.element('#vuejs').to.have.attribute('href').equals('https://vuejs.org/')
+  browser.expect.element('#vuejs').to.have.attribute('target').equals('_blank')
+  browser.expect.element('#vuejs').to.have.css('color').which.equals('rgba(66, 185, 131, 1)') // Color converted needed?
 }
